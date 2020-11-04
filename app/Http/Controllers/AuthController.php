5c18766f8a83ddf7044f850ab2f09365ca0d3a98
MP-Programming <marco.perrullo@gmail.com>
2020-11-04 19:07:34 +0100

    Adding signup, login and logout logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,23 +3,105 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthRequest;
+use App\Http\Resources\AuthResource;
 use App\Repositories\AuthRepository;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Request;
+
 class AuthController extends Controller
 {
-    protected $auth_repo;
-    public function __construct(AuthRepository $auth_repository){
-        $this->auth_repo = $auth_repository;
-    }
-    public function signUp(AuthRequest $req){
-        
-        return response()->json(compact('body'));
+  protected $auth_repo;
+  public function __construct(AuthRepository $auth_repository)
+  {
+    $this->auth_repo = $auth_repository;
+  }
+  public function signUp(AuthRequest $req)
+  {
+    try {
+      DB::beginTransaction();
+
+      /*  ***Store image in disk*** */
+
+      $image           = $req->file('image');
+      $image_path_name = time() . $image->getClientOriginalName();
+      Storage::disk('users')->put($image_path_name, File::get($image));
+
+      $data = [
+        'name'     => $req->name,
+        'surname'  => $req->surname,
+        'email'    => $req->email,
+        'password' => bcrypt($req->password),
+        'image'    => time() . $req->file('image')->getClientOriginalName(),
+      ];
+
+      $query = $this->auth_repo->create($data);
+      $user  = new AuthResource($query);
+      $body  = compact('user');
+      DB::commit();
+
+      return response()->json(compact('body'), 201);
+
+    } catch (Exception $e) {
+      DB::rollback();
+
+      $message = 'Algo inesperado aconteceu...';
+      throw new Exception($message, 500);
     }
 
-    public function login(){
-        dd('login');
+  }
+
+  public function login(Request $req)
+  {
+    $req->validate([
+      'email'       => 'required|string|email',
+      'password'    => 'required|string',
+      'remember_me' => 'boolean',
+    ]);
+
+    $credentials = request(['email', 'password']);
+
+    if (!Auth::attempt($credentials)) {
+      return response()->json([
+        'message' => 'Unauthorized',
+      ], 401);
     }
 
-    public function logout(){
+    $user = $req->user();
 
+    $tokenResult = $user->createToken('Personal Access Token');
+    $token       = $tokenResult->token;
+
+    if ($req->remember_me) {
+      $token->expires_at = Carbon::now()->addWeeks(1);
     }
+
+    $token->save();
+
+    return response()->json([
+      'access_token' => $tokenResult->accessToken,
+      'token_type'   => 'Bearer',
+      'expires_at'   => Carbon::parse(
+        $tokenResult->token->expires_at
+      )->toDateTimeString(),
+    ]);
+  }
+
+  /**
+   * Logout user (Revoke the token)
+   *
+   * @return [string] message
+   */
+  public function logout(Request $request)
+  {
+    $request->user()->token()->revoke();
+
+    return response()->json([
+      'message' => 'Successfully logged out',
+    ]);
+  }
 }
--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -15,7 +15,12 @@ class AuthResource extends JsonResource
   public function toArray($request)
   {
     return [
-      'id' => $this->id,
+      'id'      => $this->id,
+      'name'    => $this->name,
+      'surname' => $this->surname,
+      'email'   => $this->email,
+      'image'   => $this->image,
+
     ];
   }
 }
