c87c8081caba6001ee38c8a29a5a1560cb171141
MP-Programming <marco.perrullo@gmail.com>
2020-11-11 17:12:31 +0100

    Adding vue and its dependecies, also started working on login frontend

---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link :to="{ name: 'register' }">Register</router-link>
+      </li>
+       <li>
+        <router-link :to="{ name: 'login' }">Login</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
